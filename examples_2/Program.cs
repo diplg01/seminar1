74f25212136865b487513ba4bd0ce83554accb35
diplg <dimas4354@icloud.com>
2022-04-14 02:22:46 +0300

    task2

---
+++ examples_2/Program.cs
@@ -0,0 +1,11 @@
+﻿int numberA = 5;
+int numberB = 25;
+int root = numberA;
+if (numberB / numberA == root)
+{
+    Console.WriteLine($"Число {numberA} является квадратом числа {numberB}");
+}
+else
+{
+    Console.WriteLine($"Число {numberA} не является квадратом числа {numberB}");
+}
